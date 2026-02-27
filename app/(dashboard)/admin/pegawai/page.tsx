e447e25de36c85fc01ac000aee3b7c39eb1ec1c5
mihaqiel <mihaqielgithub@gmail.com>
2026-02-27 14:49:10 +0700

    fix: use client must be first in pegawai page

--- app/(dashboard)/admin/pegawai/page.tsx
+++ app/(dashboard)/admin/pegawai/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { createPegawai, deletePegawai } from "@/app/action/admin";
 import Link from "next/link";
